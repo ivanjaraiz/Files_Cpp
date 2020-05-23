597a450bbce69b84612deb132c2fdd0bda9f272f
IJM <ivan.jaraiz@gmail.com>
2020-05-24 00:53:55 +0200

    Agregar archivos de proyecto.

---
+++ Files_Cpp.cpp
@@ -0,0 +1,26 @@
+// Files_Cpp.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ofstream ofs("My.txt", ios::trunc);
+    ofs << "John" << endl;
+    ofs << 25 << endl;
+    ofs << "cs" << endl;
+
+    ofs.close(); 
+    return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
